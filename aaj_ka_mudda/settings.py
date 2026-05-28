5d0c47af34a6436fe6ddf49d27c4e54aa9b36f3a
sanjay1994singh <sanjaypython22@gmail.com>
2026-05-28 10:02:46 +0530

    p

--- aaj_ka_mudda/settings.py
+++ aaj_ka_mudda/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'aaj_ka_mudda.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'mbdb/db.sqlite3',
     }
 }
 
